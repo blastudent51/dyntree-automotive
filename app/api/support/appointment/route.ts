62e50b260f5ab1f8e310afa0c5d36a6b134bdf04
blastudent51 <student51@brighthouselearning.com>
2026-09-22 11:55:08 -0500

    Implement POST route for creating support appointments

--- app/api/support/appointment/route.ts
+++ app/api/support/appointment/route.ts
@@ -1,1 +1,66 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
 
+
+export async function POST(
+req:Request
+){
+
+ const body =
+ await req.json();
+
+
+ const vehicle =
+ await prisma.vehicle.findUnique({
+
+  where:{
+   treeId:body.treeId
+  }
+
+ });
+
+
+ if(!vehicle){
+
+  return NextResponse.json(
+   {
+    error:
+    "Vehicle not found"
+   },
+   {
+    status:404
+   }
+  );
+
+ }
+
+
+
+ const appointment =
+ await prisma.supportAppointment.create({
+
+  data:{
+   vehicleId:
+    vehicle.id,
+
+   issue:
+    body.issue,
+
+   appointment:
+    body.date
+    ?
+    new Date(body.date)
+    :
+    null
+  }
+
+ });
+
+
+
+ return NextResponse.json(
+  appointment
+ );
+
+
+}
